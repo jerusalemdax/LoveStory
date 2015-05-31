23bebc0981710fe0c4d113fb54ebc8b37abef938
jerusalemdax <jerusalemdax@gmail.com>
2015-05-31 16:49:40 +0800

    add home page

---
+++ LoveStory/Modules/HomeModules.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Nancy;
+
+public class HomeModules : NancyModule
+{
+	public HomeModules ()
+	{
+		Get ["/"] = paramters => {
+			return "This is our Love Story";
+		};
+	}
+}
