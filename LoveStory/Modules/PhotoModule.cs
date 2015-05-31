75c334a72e5bff9c7017bd2d43a1f9eaac046370
jerusalemdax <jerusalemdax@gmail.com>
2015-05-31 16:58:50 +0800

    add photo module

---
+++ LoveStory/Modules/PhotoModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Nancy;
+
+public class PhotoModule : NancyModule
+{
+	public PhotoModule () : base("/photo")
+	{
+		Get ["/{slug}"] = paramters => {
+			return string.Format("photho :" + paramters.slug);
+		};
+	}
+}
