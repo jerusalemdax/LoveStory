78c89ba9e4870acf5c4f3334c48d7fa94a6ad638
jerusalemdax <jerusalemdax@gmail.com>
2015-06-04 14:09:46 +0800

    add postgresql test

--- LoveStory/Modules/HomeModules.cs
+++ LoveStory/Modules/HomeModules.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Nancy;
+using Simple.Data;
 
 public class HomeModules : NancyModule
 {
 	public HomeModules ()
 	{
 		Get ["/"] = paramters => {
-			return View["index"];
+            var db = Database.OpenConnection("Server=127.0.0.1;Port=5432;Database=LoveStory;Integrated Security=true");
+            //var user = db.Public.Users.FindByName("daniel");
+            return "find user";// +user.Password;
 		};
 	}
 }
