b36ed8ddf6f0c31b0fccac253de8c81b1f5d70e8
jerusalemdax <jerusalemdax@gmail.com>
2015-06-05 16:41:28 +0800

    mysql ok

--- LoveStory/Modules/HomeModules.cs
+++ LoveStory/Modules/HomeModules.cs
@@ -7,9 +7,10 @@ public class HomeModules : NancyModule
 	public HomeModules ()
 	{
 		Get ["/"] = paramters => {
+			StaticConfiguration.DisableErrorTraces = false;
             var db = Database.OpenConnection("Server=localhost;Port=3306;Database=LoveStory;Uid=username;Pwd=password");
             var user = db.Public.Users.FindByName("daniel");
-            return "find user" + user.Password;
+			return "find user" + user.Password;
 		};
 	}
 }
