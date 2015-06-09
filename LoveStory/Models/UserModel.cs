688924179446e56bdf9100500a74c1cc8c4c0a4d
jerusalemdax <jerusalemdax@gmail.com>
2015-06-09 14:01:05 +0800

    add User Model

---
+++ LoveStory/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class UserModel
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public UserModel(string username, string password)
+    {
+        this.Username = username;
+        this.Password = password;
+    }
+
+}
